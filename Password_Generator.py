508cab6dfe89927480e5cdef820d2f2ddd590c54
Danio <85469257+DanioLive@users.noreply.github.com>
2022-01-26 12:58:15 -0800

    Add files via upload

---
+++ Password_Generator.py
@@ -0,0 +1,13 @@
+import random
+
+char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","@","#","$","%","^","&","*","1","2","3","4","5","6","7","8","9","0"]
+char1 = (random.choice(char))
+char2 = (random.choice(char))
+char3 = (random.choice(char))
+char4 = (random.choice(char))
+char5 = (random.choice(char))
+char6 = (random.choice(char))
+char7 = (random.choice(char))
+char8 = (random.choice(char))
+char9 = (random.choice(char))
+print("Your password is:" + char1+char2+char3+char4+char5+char6+char7+char8+char9)
